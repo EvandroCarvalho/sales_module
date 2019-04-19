978437835772b8e2fb8d6105fe7cc8346d99ffda
Evandro Carvalho <evandrovjunior@gmail.com>
2019-04-19 00:47:02 -0300

    configuração dos ambientes

--- src/main/java/br/com/salesModule/config/DataSourceConfig.java
+++ src/main/java/br/com/salesModule/config/DataSourceConfig.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -13,6 +14,7 @@ import java.net.URISyntaxException;
 public class DataSourceConfig {
 
     @Bean
+    @Profile("heroku")
     public BasicDataSource dataSource() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
