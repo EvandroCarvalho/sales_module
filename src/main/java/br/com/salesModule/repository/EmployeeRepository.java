1e3a3f64e10846ad9b2c69ec44a0aa3eb962a6d9
Evandro Carvalho <evandrovjunior@gmail.com>
2019-05-10 14:55:39 -0300

    add cors para employee

--- src/main/java/br/com/salesModule/repository/EmployeeRepository.java
+++ src/main/java/br/com/salesModule/repository/EmployeeRepository.java
@@ -2,9 +2,11 @@ package br.com.salesModule.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import br.com.salesModule.model.Employee;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
